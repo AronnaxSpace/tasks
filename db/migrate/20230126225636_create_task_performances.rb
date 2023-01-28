1c3114503a3752883296d6e21d6ade45049750b3
ihortok <itokaryk@gmail.com>
2023-01-28 02:19:46 +0200

    fix migrations

--- db/migrate/20230126225636_create_task_performances.rb
+++ db/migrate/20230126225636_create_task_performances.rb
@@ -1,4 +1,4 @@
-class CreateTaskEvents < ActiveRecord::Migration[7.0]
+class CreateTaskPerformances < ActiveRecord::Migration[7.0]
   def change
     create_table :task_performances do |t|
       t.belongs_to :task, null: false, foreign_key: true
